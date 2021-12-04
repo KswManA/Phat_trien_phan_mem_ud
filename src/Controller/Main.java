43151e1d28c4acbf4297896200177272fc0b8841
KswManA <85831975+KswManA@users.noreply.github.com>
2021-12-04 08:05:20 +0700

    Add files via upload

---
+++ src/Controller/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.Asset_View;
+
+/**
+ *
+ * @author HuyLQ
+ */
+public class Main {
+    public static void main(String[] args) {
+        Asset_View view = new Asset_View();
+        AssetController cl = new AssetController();
+        
+    }
+}
